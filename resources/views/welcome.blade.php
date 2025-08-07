c5c44977a375a510196ba9559f206c5dcfd6cc17
muchamadhatta <muchamadhatta96@gmail.com>
2025-08-08 06:40:16 +0700

    tambah favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Songchord - Your Musical Companion</title>
-    
+    <!-- Favicon -->
+    <link rel="icon" type="image/svg+xml" href="{{ asset('music_favicon.svg') }}">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
